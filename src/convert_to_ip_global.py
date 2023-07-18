a283aeb3b1d84200c0210c0d76974eb6d4d1b6e9
9FS <pray4spam@googlemail.com>
2023-07-18 20:39:31 +0000

    moved converting domain or IP to global IP into separate function; added IP command

---
+++ src/convert_to_ip_global.py
@@ -0,0 +1,50 @@
+#Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+import inspect
+import ipaddress
+import logging
+import requests
+import socket
+
+
+def convert_to_ip_global(ip_or_domain: str) -> ipaddress.IPv4Address|ipaddress.IPv6Address:
+    """
+    Converts given IP or domain, which may me local or global, to a guaranteed global IP using "https://ident.me/".
+
+    Arguments:
+    - ip_local: IP to convert
+
+    Returns:
+    - ip_global: converted IP, guaranteed global
+
+    Raises:
+    - ValueError: IP or domain cannot be converted to IP object.
+    - TimeoutError: Converting local IP to global IP via "https://ident.me/" timed out.
+    """
+
+    ip_local_or_global: ipaddress.IPv4Address|ipaddress.IPv6Address #ip which may be local or global
+    ip_global: ipaddress.IPv4Address|ipaddress.IPv6Address          #ip that is guaranteed global, result
+    TIMEOUT: int=50                                                 #internet connection timeout
+
+
+    logging.info(f"Converting given IP or domain \"{ip_or_domain}\" to IP...")
+    try:
+        ip_local_or_global=ipaddress.ip_address(socket.gethostbyname(ip_or_domain)) #convert domain to IP, construct IP object
+    except socket.gaierror:
+        logging.error(f"\rConverting given IP or domain \"{ip_or_domain}\" to IP failed. Unable to get IP address information. Check the given domain/IP and the internet connection.")
+        raise ValueError(f"Error in {convert_to_ip_global.__name__}{inspect.signature(convert_to_ip_global)}: Converting given IP or domain \"{ip_or_domain}\" to IP failed. Unable to get IP address information. Check the given domain/IP and the internet connection.")
+    logging.info(f"\rConverted given IP or domain \"{ip_or_domain}\" to IP \"{ip_local_or_global.exploded.upper()}\".")
+
+    if ip_local_or_global.is_global==True:  #if already global IP: work done, return
+        ip_global=ip_local_or_global
+        return ip_global
+
+    logging.info(f"Converting local IP \"{ip_local_or_global.exploded.upper()}\" to global IP...")
+    try:
+        ip_global=ipaddress.ip_address(requests.get("https://ident.me/", timeout=TIMEOUT).text) #convert to global IP
+    except TimeoutError:
+        logging.error(f"\rConverting local IP \"{ip_local_or_global.exploded.upper()}\" to global IP timed out.")
+        raise
+    logging.info(f"\rConverted local IP \"{ip_local_or_global.exploded.upper()}\" to global IP \"{ip_global.exploded.upper()}\".")
+
+
+    return ip_global
--- src/main.py
+++ src/main.py
@@ -5,21 +5,21 @@ import ipaddress
 import KFS.config, KFS.fstr, KFS.log
 import logging
 import mcstatus, mcstatus.pinger
-import requests
-import socket
 import time
+from convert_to_ip_global import convert_to_ip_global
 
 
 @KFS.log.timeit
 def main() -> None:
     discord_bot: discord.ext.commands.Bot   #discord bot instance
+    DISCORD_BOT_CHANNEL_NAME: str="botspam" #channel to monitor for ip command
     discord_bot_token: str                  #discord bot token
     intents: discord.Intents                #bot permissions
     REFRESH_FREQUENCY: float=200e-3         #refresh display with 200mHz (every 5s)
-    TIMEOUT: int=50                         #internet connection timeout
     
     discord_bot_token=KFS.config.load_config("discord_bot.token")               #load discord bot token
     intents=discord.Intents.default()                                           #standard permissions
+    intents.message_content=True                                                #in addition with message contents
     discord_bot=discord.ext.commands.Bot(command_prefix="", intents=intents)    #create bot instance
 
 
@@ -32,8 +32,9 @@ def main() -> None:
     @discord.ext.tasks.loop(seconds=1/REFRESH_FREQUENCY)
     async def refresh(discord_bot: discord.ext.commands.Bot) -> None:    #refresh display regurarily
         """
-        refresh bot display
+        Refreshes bot presence with current number of players online, maximum number of players, player names, and server IP. Also sets status to online or offline as appropiate.
         """
+
         discord_presence_title: str                                             #presence, important information
         discord_status: discord.Status                                          #current status, online (green) for server online, do not disturb (red) for server offline
         minecraft_server: mcstatus.JavaServer                                   #server instance
@@ -42,36 +43,25 @@ def main() -> None:
         minecraft_server_port: int                                              #target server port
         minecraft_server_status: mcstatus.pinger.PingResponse                   #server status
 
-        minecraft_server_ip_port=KFS.config.load_config("minecraft_server_ip.config").split(":")        #load set server IP/domain and port, may be local or global
-        
+
+        minecraft_server_ip_port=KFS.config.load_config("minecraft_server_ip.config").rsplit(":", 1)    #load set server IP/domain and port, may be local or global
         minecraft_server_ip_user=minecraft_server_ip_port[0]
-        logging.info(f"Converting configured IP \"{minecraft_server_ip_user}\" to global IP...")
-        try:
-            minecraft_server_ip_global=ipaddress.ip_address(socket.gethostbyname(minecraft_server_ip_user)) #convert to IP
-        except socket.gaierror:
-            logging.error(f"\rConverting configured IP \"{minecraft_server_ip_user}\" to global IP failed. Unable to get IP address information. Check the given domain/IP and the internet connection.")
-            return
-        if minecraft_server_ip_global.is_private==True:                                             #if not global IP:
-            try:
-                minecraft_server_ip_global=ipaddress.ip_address(requests.get("https://ident.me/", timeout=TIMEOUT).text)    #convert to global IP
-            except TimeoutError:
-                logging.error(f"\rConverting configured IP \"{minecraft_server_ip_user}\" to global IP timed out.")
-                return
-        logging.info(f"\rConverted configured IP \"{minecraft_server_ip_user}\" to global IP \"{minecraft_server_ip_global.exploded.upper()}\".")
+        minecraft_server_ip_global=convert_to_ip_global(minecraft_server_ip_user)
         try:
             minecraft_server_port=int(minecraft_server_ip_port[1])
         except ValueError:
             logging.error(f"Converting given port \"{minecraft_server_ip_port[1]}\" to int failed.")
             return
         
+
         minecraft_server=mcstatus.JavaServer.lookup(f"{minecraft_server_ip_user}:{minecraft_server_port}")
         logging.info(f"Fetching server status at \"{minecraft_server_ip_user}:{minecraft_server_port}\"...")
         try:
-            minecraft_server_status=minecraft_server.status()       #current server status
-        except (IOError, TimeoutError):                             #if server currently transitioning between online offline or is currently offline:
+            minecraft_server_status=minecraft_server.status()   #current server status
+        except (IOError, TimeoutError):                         #if server currently transitioning between online offline or is currently offline:
             logging.info(f"\rFetching server status at \"{minecraft_server_ip_user}:{minecraft_server_port}\" failed. Server is assumed to be offline.")
-            discord_presence_title="offline"                        #just say it's offline
-            discord_status=discord.Status.do_not_disturb            #status red
+            discord_presence_title="offline"                    #just say it's offline
+            discord_status=discord.Status.do_not_disturb        #status red
         else:                                                                                                                           #if server online:
             logging.info(f"\rFetched server status at \"{minecraft_server_ip_user}:{minecraft_server_port}\" with latency {KFS.fstr.notation_tech(minecraft_server_status.latency/1000, 2)}s.")
             discord_presence_title=f"{minecraft_server_status.players.online}/{minecraft_server_status.players.max}"                    #player numbers online
@@ -79,13 +69,53 @@ def main() -> None:
                 discord_presence_title+=f": {', '.join(sorted([player.name for player in minecraft_server_status.players.sample]))}"    #append player name list #type:ignore
             discord_status=discord.Status.online                                                                                        #status green
         discord_presence_title+=f"; IP: {minecraft_server_ip_global.exploded.upper()} :{minecraft_server_port}"                         #display, separate ip and port with space because IPv6 is so long and display needs a linebreak
-        logging.info(f"Discord bot status: {discord_status}")
-        logging.info(f"Presence title: {discord_presence_title}")
 
-        logging.info(f"Applying presence title and bot status...")
+        logging.info(f"Applying presence title \"{discord_presence_title}\" and bot status \"{discord_status.name}\"...")
         await discord_bot.change_presence(activity=discord.Activity(name=discord_presence_title, type=discord.ActivityType.playing),    #apply presence
                                           status=discord_status,)
-        logging.info(f"\rApplied presence title and bot status.")
+        logging.info(f"\rApplied presence title \"{discord_presence_title}\" and bot status \"{discord_status.name}\".")
+        return
+    
+
+    @discord_bot.event
+    async def on_message(message: discord.Message):
+        """
+        Executed every time a message is sent on the server. If the message is not from the bot itself and in a bot channel, process it.
+
+        - \"ip\" sends the current IP for copy and paste purposes.
+        """
+
+        message_send: str                                                       #message to send to discord
+        minecraft_server_ip_global: ipaddress.IPv4Address|ipaddress.IPv6Address #target server IP, global
+        minecraft_server_ip_user: str                                           #target server IP as defined by user, local or global
+        minecraft_server_port: int                                              #target server port
+
+        if(   message.author==discord_bot.user    #if message from bot itself or outside dedicated bot channel or not the right command: do nothing 
+           or message.channel.name!=DISCORD_BOT_CHANNEL_NAME    #type:ignore
+           or message.content.upper()!="IP"):
+            return
+        logging.info("Executing IP command...")
+
+        minecraft_server_ip_port=KFS.config.load_config("minecraft_server_ip.config").rsplit(":", 1)    #load set server IP/domain and port, may be local or global
+        minecraft_server_ip_user=minecraft_server_ip_port[0]
+        minecraft_server_ip_global=convert_to_ip_global(minecraft_server_ip_user)
+        try:
+            minecraft_server_port=int(minecraft_server_ip_port[1])
+        except ValueError:
+            logging.error(f"Converting given port \"{minecraft_server_ip_port[1]}\" to int failed.")
+            return
+
+        message_send=f"[{minecraft_server_ip_global.exploded.upper()}]:{minecraft_server_port}"
+        logging.info(f"Sending message \"{message_send}\" to discord...")
+        try:
+            await discord_bot.get_channel(message.channel.id).send(message_send)    #send message to discord #type:ignore
+        except AttributeError:                                                      #get_channel already returned None, bot has probably been removed from server
+            logging.error(f"Sending message \"{message_send}\" to discord failed with AttributeError.")
+        except discord.errors.DiscordServerError:                                   #send failed
+            logging.error(f"Sending message \"{message_send}\" to discord failed with discord.errors.DiscordServerError.")
+        else:
+            logging.info(f"\rSent message \"{message_send}\" to discord.")
+
         return
     
 
