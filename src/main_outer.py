382ac6ccbd882255e75bc411f4c1ff40d927d938
9FS <pray4spam@googlemail.com>
2023-09-16 10:08:33 +0000

    updated KFS; updated main_outer.py

--- src/main_outer.py
+++ src/main_outer.py
@@ -1,18 +1,16 @@
-#Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
-import KFS.log      #setup logging
-import logging      #standard logging
-import traceback    #exception message full when program crashes as .exe
+# Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+import KFS.log
+import logging
+import traceback
 from main import main
 
 
 KFS.log.setup_logging("", logging.INFO)
-#KFS.log.setup_logging("", logging.DEBUG)
+#KFS.log.setup_logging("", logging.DEBUG, filepath_format="./log/%Y-%m-%dT%H_%M.log", rotate_filepath_when="M")
+
 try:
     main()
 except:
     logging.critical(traceback.format_exc())
-    
-    print("\n\nPress enter to close program.", flush=True)
-    input() #pause
-else:
-    print("\n", end="", flush=True)
+    print("\nPress enter to close program.", flush=True)
+    input() # pause
