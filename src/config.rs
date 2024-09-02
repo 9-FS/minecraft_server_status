2036510c10a25062df9db264d56ee5cd4feec968
9FS <pray4spam@googlemail.com>
2024-09-02 13:49:16 +0000

    updated formatting

--- src/config.rs
+++ src/config.rs
@@ -8,21 +8,22 @@ use crate::domain_or_ip::*;
 #[allow(non_snake_case)]
 pub struct Config
 {
-    pub DISCORD_BOT_TOKEN:             String,      // discord bot token
-    pub MINECRAFT_SERVER_DOMAIN_OR_IP: DomainOrIp,  // minecraft server domain or ip, do not add port here
-    pub MINECRAFT_SERVER_PORT:         Option<u16>, // minecraft server port, optional
-    pub REFRESH_INTERVAL:              u64,         // refresh display every `REFRESH_INTERVAL` seconds
+    pub DISCORD_BOT_TOKEN: String, // discord bot token
+    pub MINECRAFT_SERVER_DOMAIN_OR_IP: DomainOrIp, // minecraft server domain or ip, do not add port here
+    pub MINECRAFT_SERVER_PORT: Option<u16>, // minecraft server port, optional
+    pub REFRESH_INTERVAL: u64, // refresh display every `REFRESH_INTERVAL` seconds
 }
 
 impl Default for Config
 {
     fn default() -> Self
     {
-        Config {
-            DISCORD_BOT_TOKEN:             "".to_owned(),
+        Config
+        {
+            DISCORD_BOT_TOKEN: "".to_owned(),
             MINECRAFT_SERVER_DOMAIN_OR_IP: DomainOrIp::Domain("".to_owned()),
-            MINECRAFT_SERVER_PORT:         Some(25565),
-            REFRESH_INTERVAL:              5,
+            MINECRAFT_SERVER_PORT: Some(25565),
+            REFRESH_INTERVAL: 5,
         }
     }
 }
